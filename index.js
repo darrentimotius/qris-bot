c323d7bfd7f2583ed29f23da559821c8c62fa4ca
darrentimotius <timotiusdarren@gmail.com>
2025-11-07 14:44:17 +0700

    Set up for deploy in vercel

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import { config } from './config.js';
-import bot, { setWebHook } from './bot.js';
+import bot, { setWebhook } from './bot.js'; 
 
 const app = express();
 app.use(express.json());
 
 app.post(config.webhookPath, (req, res) => {
-  bot.processUpdate(req.body);
+  bot.processUpdate(req.body); 
   res.sendStatus(200);
 });
 
 app.get('/', (req, res) => {
-  res.send('Bot QRIS (Vercel) sedang berjalan!');
+  res.send('Bot QRIS sedang berjalan!');
 });
 
 app.get('/register-webhook', async (req, res) => {
   try {
-    await bot.setWebHook(config.webhookUrl); 
+    await setWebhook(); 
     res.status(200).send(`Webhook berhasil di-set ke: ${config.webhookUrl}`);
   } catch (error) {
     console.error("Gagal set webhook:", error.message);
